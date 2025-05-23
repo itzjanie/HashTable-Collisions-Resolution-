18797961261c40e187321072d01d97aefef436d2
itzjanie <fu.janie.huijia_2025@nyjc.edu.sg>
2025-05-23 04:28:05 +0000

    test

--- hashtable_open_addressing.py
+++ hashtable_open_addressing.py
@@ -1,6 +1,7 @@
 class HashTable:
     def __init__(self, size):
-        pass
+        self.size = size
+        self.values = [None] * self.size
 
     def __repr__(self):
         """returns a formatted string containing the values in the hash table"""
@@ -11,15 +12,46 @@ class HashTable:
         return hash(key) % self.size
       
     def _rehash(self, old_location: int) -> int:
-	""" Compute and returns the next location for linear probing """
-   	pass
-
+        """
+        computes and returns the new location for linear probing
+        """
+        return (old_location+1) % self.size
+    
     def setitem(self, key: str, value: dict) -> None:
         """
+        adds or updates a key-value pair in the list and raises an exception if the table is full
         """
-    	pass
+        location = self._hash(key)
+        old_location = location
+        if self.values[location] == None:
+            self.values[location] = [key, value]
+        elif self.values[location][0] == key:
+            self.values[location] = [key, value]
+        else:
+            while self.values[location] != None:
+                if self.values[location][0] != key:
+                    location = self._rehash(location)
+                elif location == old_location:
+                    print('The hashtable is full. Please try again.')
+                    return False
+            self.values[location] = [key, value]
+            
+    
 
     def getitem(self, key: str) -> 'dict | None':
         """
+        return the value of the associated with the key and raise a Keyerror if the key is not found
         """
-    	pass
+        location = self._hash(key)
+        old_location = location
+        if self.values[location] == None:
+            raise KeyError
+        elif self.values[location][0] == key:
+            return self.values[location][1]
+        else:
+            while self.values[location][0] != key:
+                location = self._rehash(location)
+                if location == old_location:
+                    raise KeyError
+            return self.values[location][1]    
+            
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from hashtable_open_addressing import HashTable
 
 if __name__ == "__main__":
     ht = HashTable(30)
+    with open('student_data.csv', 'r') as fhand:
+        fhand.readline()
+        for line in fhand:
+            line = line.strip().split(',')
+            id = line[0]
+            data = line[1:]
+            ht.setitem(id, data)
+    print(ht)
     """
     1. Extract the records from the student_data file
     and add them one at a time, as a Python dict, 
@@ -14,3 +22,8 @@ if __name__ == "__main__":
     
     # Test your hashtable using appropriate methods
     # from your implementation
+    # ht = HashTable(10)
+    # records = {'id': 1234, 'name': 'john'}
+    # ht.setitem('id', records)
+    # print(ht)
+    # print(ht.getitem('ie'))
